ba7c5eae9d9fb1148c6f44b3d41f5bbf2089fc80
Fuganti <juliocesarfuganti@gmail.com>
2025-09-20 13:40:48 -0300

    fix: scrapper removed

--- src/agent_workflow/graph/graph.py
+++ src/agent_workflow/graph/graph.py
@@ -4,31 +4,26 @@ from langchain_core.prompts import ChatPromptTemplate
 from langgraph.graph import StateGraph, END
 from typing import TypedDict, List, Tuple, Dict
 
-from html_scrapp import scrap
-
 class ChatState(TypedDict):
-    url: str
+    post_text: str
     output: str
     toos_info: Dict
     score: float
 
-def separate_html_text_node(state: ChatState, google_api_key: str):
-    url_info = scrap(state['url'])
-    llm = ChatGoogleGenerativeAI(temperature=0, model="gemini-flash-lite-2.5", api_key=google_api_key)
-
 def search_related_news_node(state: ChatState):
-    pass
+    print("search_related_news_node")
 
 def info_check_node(state: ChatState, openai_api_key: str):
-    llm = ChatOpenAI(temperature=0, model="gpt-4", api_key=openai_api_key)
+    #llm = ChatOpenAI(temperature=0, model="gpt-4", api_key=openai_api_key)
+    print("info_check_node")
 
 def final_answer_node(state: ChatState, openai_api_key: str):
-    llm = ChatOpenAI(temperature=0, model="gpt-4", api_key=openai_api_key)
+    #llm = ChatOpenAI(temperature=0, model="gpt-4", api_key=openai_api_key)
+    print("final_answer_node")
 
-def graph(google_api_key: str, openai_api_key:str) -> StateGraph[ChatState]:
+def graph(google_api_key = '', openai_api_key =  '') -> StateGraph[ChatState]:
     builder = StateGraph(ChatState)
 
-    builder.add_node("separate_html_text", lambda state: separate_html_text_node(state, google_api_key))
     builder.add_node("search_related_news", search_related_news_node)
     builder.add_node("info_check", lambda state: info_check_node(state, openai_api_key))
     builder.add_node("final_answer", lambda state: final_answer_node(state, openai_api_key))
--- src/main.py
+++ src/main.py
@@ -1,27 +1,29 @@
 from contextlib import asynccontextmanager
 from fastapi import FastAPI, Query, Request
+import os
 
-#from agent_workflow.graph.graph import graph, ChatState
+from src.agent_workflow.graph.graph import graph, ChatState
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     # google_api_key = os.getenv("GOOGLE_API_KEY") 
-    # app.state.graph = graph(google_api_key, )
+    app.state.graph = graph()
     yield
 
 app = FastAPI(title="Unfaker API", version="1.0.0", lifespan=lifespan)
 
 @app.get("/scan", tags=["scanning"])
-async def scan(request: Request,url: str = Query(...)):
-    return {
-        "url": url,
-        "score": 0.5,
-        "similar_articles": ["https://site1.com", "https://site2.com"],
-        "critical_points": [{"trusty": "Test", "tested": "Test"}]
-    }
-    # graph = request.app.state.graph 
+async def scan(request: Request,post_text: str = Query(...)):
+    graph = request.app.state.graph 
 
-    # result = await graph.ainvoke(ChatState(url=url))
+    result = await graph.ainvoke(ChatState(post_text=post_text))
+
+    return {
+         "post_text": result.get("post_text", ""),
+         "score": 0.5,
+         "similar_articles": ["https://site1.com", "https://site2.com"],
+         "critical_points": [{"trusty": "Test", "tested": "Test"}]
+    }
     # tools_info = result.get("tools_info", [])
     # similar_articles = tools_info.get("similar_articles", []) 
     # critical_points = tools_info.get("critical_points", [])
