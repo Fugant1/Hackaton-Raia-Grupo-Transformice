0231a6b5c3c3da1f38a03f76de94443363c64ead
Fuganti <juliocesarfuganti@gmail.com>
2025-09-20 13:51:38 -0300

    fix: url to post_text

--- src/agent_workflow/graph/graph.py
+++ src/agent_workflow/graph/graph.py
@@ -27,8 +27,7 @@ def graph(google_api_key = '', openai_api_key =  '') -> StateGraph[ChatState]:
     builder.add_node("search_related_news", search_related_news_node)
     builder.add_node("info_check", lambda state: info_check_node(state, openai_api_key))
     builder.add_node("final_answer", lambda state: final_answer_node(state, openai_api_key))
-    builder.set_entry_point("separate_html_text")
-    builder.add_edge("separate_html_text", "search_related_news")
+    builder.set_entry_point("search_related_news")
     builder.add_edge("search_related_news", "info_check")
     builder.add_edge("info_check", "final_answer")
 
